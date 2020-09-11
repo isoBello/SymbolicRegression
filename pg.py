3095e3fd880b93b70e612417de3ff353bf0de40c
isoBello <isabellamfreitas2@gmail.com>
2020-09-10 22:16:12 -0300

    Correção dos erros de fitness

--- pg.py
+++ pg.py
@@ -5,7 +5,8 @@ import math
 import logging
 import numpy as np
 import matplotlib.pyplot as plot
-import sys
+
+# import sys
 
 # Constants used to the experimental analysis of the PG
 executions = 10
@@ -13,9 +14,9 @@ depth_individual = 7
 alfa_crossover = 0.9
 beta_mutation = 0.05
 tournament_size = 2
-population_size = 50
+population_size = 10
 elite_individuals = 2
-generations = 100
+generations = 20
 optimal_fitness = 0.0
 
 # Variables used to present the results of the PG
@@ -23,25 +24,27 @@ best_individual = None
 best_fitness = 0.0
 worst_fitness = 0.0
 average_fitness = 0.0
+
 best_evolution = []
 average_evolution = []
 worst_evolution = []
-best_children = []
+children_evolution = []
+
 last_generation = 0.0
 crossover_count = 0
-best_children_gen = []
-best_individuals_crossover = []
+number_best_children = 0
+proportion_best_children = 0
 number_data = 1
 
 # Tree characteristics
 operator = "Operator"
 constant = "Constant"
 variable = "Variable"
-functions = {'add': 1, 'sub': 2, 'mul': 3, 'div': 4, 'sen': 5, 'coss': 6}
+functions = {1: 'add', 2: 'sub', 3: 'mul', 4: 'div', 5: 'sen', 6: 'cos'}
+functions_size = len(functions)
 variables = {'x': 1}
 variables_size = len(variables)
 interval_constants = range(-10, 10)
-functions_size = 4
 depth_max = 6
 depth_min = 2
 
@@ -71,21 +74,21 @@ class Node:
             return self.function_evaluation(self.value, self.leaf_left.evaluation(values),
                                             self.leaf_right.evaluation(values))
         elif self.type == variable:
-            return values[self.value]
+            return values[self.value - 1]
 
     @staticmethod
     def function_evaluation(opration, a, b):
-        if opration == functions['add']:
+        if opration == 1:
             return a + b
-        elif opration == functions['sub']:
+        elif opration == 2:
             return a - b
-        elif opration == functions['mul']:
+        elif opration == 3:
             return a * b
-        elif opration == functions['div']:
+        elif opration == 4:
             return a / b if b else 0  # Prevents errors from division: num / 0
-        elif opration == functions['sen']:
+        elif opration == 5:
             return math.sin(a)
-        elif opration == functions['coss']:
+        elif opration == 6:
             return math.cos(b)
 
     def subtree(self, depth):
@@ -110,6 +113,9 @@ class Node:
                     else:
                         return self
 
+    def swap_subtree(self, b):  # Swap between two nodes
+        self.__dict__, b.__dict__ = b.__dict__, self.__dict__
+
     def tree_mutation(self, max_depth, index):
         type_mutation = randint(0, 2)
         if type_mutation == 0:  # Mutate into a variable
@@ -124,30 +130,29 @@ class Node:
             self.leaf_right = None
         elif type_mutation == 2:  # Mutate into a function
             if self.height == max_depth:
-                new_tree = self.change_subtree(depth_max, depth=self.height)
+                new_tree = tree(self.height, '')
                 Node.swap_subtree(self, new_tree)
 
-    def swap_subtree(self, b):  # Swap between two nodes
-        self.__dict__, b.__dict__ = b.__dict__, self.__dict__
-
     def print_tree(self, level):
-        string_tree = ""
+        str_tree = '  ' * level
         if self.type == variable:
-            string_tree += "x" + str(self.value)
+            str_tree += "x" + str(self.value)
         elif self.type == operator:
-            string_tree += str(get_operator(self.value))
+            str_tree += str(get_operator(self.value))
         else:
-            string_tree += str(self.value)
-        string_tree += "\n"
+            str_tree += str(f'{self.value:.4g}')
+
         if self.leaf_left is not None:
-            string_tree += "Left Node" + str(self.leaf_left.height) + " -> " + self.leaf_left.print_tree(level + 1)
+            str_tree += self.leaf_left.print_tree(level + 1)
         if self.leaf_right is not None:
-            string_tree += "Right Node" + str(self.leaf_right.height) + " -> " + self.leaf_right.print_tree(level + 1)
+            str_tree += self.leaf_right.print_tree(level + 1)
+
+        return str_tree
 
 
 class Individual:
     def __init__(self, method):
-        self.tree = tree(max_depth_individual, min_depth_individual, index_individual, method)
+        self.tree = tree(max_depth_individual, method)
         self.max_depth = max_depth_individual
         self.min_depth = min_depth_individual
         self.index = index_individual
@@ -177,42 +182,49 @@ class Individual:
             b = snd_parent.tree.subtree(depth)
 
             Node.swap_subtree(a, b)
-            return a, b
+            a_ind = Individual(a)
+            b_ind = Individual(b)
+            return a_ind, b_ind
         else:
             return None, None
 
     def print_individual(self):
-        return "Tree - {}".format(self.print_tree(0), end="\nFitness - {}".format(str(self.fitness)))
+        string_individual = "Tree " + self.tree.print_tree(0) + "\nFitness " + str(self.fitness)
+        return string_individual
 
 
 # Begin of the actual algorithm
 def tree(depth, method):
-    if depth == depth_max:
-        return node(variables_size, depth)
-    else:
-        type_nodo = randint(0, 1)
-        if type_nodo == 0 and depth_min <= depth < depth_max and method == "full":
-            return node(variables_size, depth)
+    # Method defines if it will be a full or grow tree. If True = Full, otherwise = Grow. Based on ramped-half-and-half
+    try:
+        if depth == depth_max:
+            return node(depth)
         else:
-            operation = randint(1, functions_size)
-            left = tree(depth_max, depth_min, variables_size, depth + 1)
-            right = tree(depth_max, depth_min, variables_size, depth + 1)
-            format_tree = Node(operator, operation, depth, left, right)
-            return format_tree
+            type_nodo = rand_index(0, 1)
+            if type_nodo == 0 and depth_min <= depth and not method:
+                return node(depth)
+            else:
+                operation = rand_index(1, functions_size)
+                left = tree(depth + 1, method)
+                right = tree(depth + 1, method)
+                format_tree = Node(operator, operation, depth, left, right)
+        return format_tree
+    except Exception as e:
+        logging.exception(e)
 
 
-def node(index, depth):  # Return a new node, who will be const or var
-    type_nodo = randint(0, 1)
-    if type_nodo == 0:
-        return Node(variable, randint(1, index), depth)
+def node(depth):  # Return a new node, who will be const or var
+    no = randint(0, 1)
+    if no == 0:
+        return Node(variable, rand_index(1, variables_size), depth)
     else:
         return Node(constant, random(), depth)
 
 
-def get_operator(value):
+def get_operator(key):
     for k, v in functions.items():
-        if v == value:
-            return k
+        if k == key:
+            return v
 
 
 def rand_index(a, b):
@@ -225,32 +237,34 @@ def fitness_calculator(individual, values):
     abs_error = 0.0
     index = len(values[0])
     for v in values:
-        fitness_individual = individual.tree.evaluation(values[:index - 1])
+        fit = individual.tree.evaluation(v[:index - 1])
         # The last index in the list is the f(x) himself
-        expected_fitness = values[index - 1]
-        abs_error += np.abs(fitness_individual - expected_fitness)
+        expected_fitness = v[index - 1]
+        abs_error += np.abs(fit - expected_fitness)
         return abs_error
 
 
 def read_data():
     data = []
-    with open(sys.argv[1], "rb") as archive:
+    temp = []
+    with open("datasets-TP1/SR_circle.txt", "r") as archive:
         lines = archive.readlines()
         for line in lines:
             values = line.split()
             for v in values:
-                values.append(float(v))
-            data.append(values)
+                v = float(v)
+                temp.append(v)
+            data.append(temp)
     return data
 
 
-def create_population():
+def create_population(size):
     individuals = []
-    for _ in range(0, (population_size/2)):
-        new_individual = Individual()
+    for _ in range(0, int(size / 2)):
+        new_individual = Individual(method='')
         individuals.append(new_individual)
     # Population size needs to always be a pair number
-    for _ in range(0, (population_size/2)):
+    for _ in range(0, int(size / 2)):
         new_individual = Individual(method="full")
         individuals.append(new_individual)
     return individuals
@@ -276,40 +290,33 @@ def do_tournament(population):
 def statistics(population):
     population_s = sorted(population, key=lambda k: k.fitness)
     global best_individual, best_fitness, worst_fitness, average_fitness, best_evolution, average_evolution, \
-        worst_evolution, best_children, crossover_count, best_children_gen, best_individuals_crossover
+        worst_evolution, children_evolution, crossover_count, number_best_children, proportion_best_children
 
     best_fitness = population_s[0].fitness
     best_individual = population_s[0]
     worst_fitness = population_s[-1].fitness
     average_fitness = np.average([k.fitness for k in population_s])
     if crossover_count > 0.0:
-        best_individuals_crossover = (best_children / (crossover_count * 2.0)) * 100
+        proportion_best_children = (number_best_children / (crossover_count * 2.0)) * 100
 
     best_evolution.append(best_fitness)
     average_evolution.append(average_fitness)
     worst_evolution.append(worst_fitness)
-    best_children_gen.append(best_individuals_crossover)
+    children_evolution.append(proportion_best_children)
 
 
 def run():
-    global crossover_count, best_children, last_generation, number_data
-    best_children = 0.0
+    global crossover_count, number_best_children, last_generation, number_data
+    number_best_children = 0.0
     last_generation = 0.0
     n = 1
 
     data = read_data()
     number_data = len(data)
 
-    # Write Output
-    with open(sys.argv[2], "w") as archive:
-        archive.write("Variáveis analisadas: " + str(number_data) + "\n")
-        archive.write("Tamanho da População: " + str(population_size) + "\n")
-        archive.write("Probabilidade - Crossover: " + str(alfa_crossover) + "\n")
-        archive.write("Probabilidade - Mutação: " + str(beta_mutation) + "\n")
-        archive.write("Tamanho - Torneio: " + str(tournament_size) + "\n")
-        archive.write("Geração > Melhor Fitness > Fitness Médio > Melhor Indivíduo")
+    write_output()
 
-    population = create_population()
+    population = create_population(population_size)
     for each in population:
         each.fitness_individual(data)
 
@@ -322,20 +329,21 @@ def run():
         new_population = []
         last_best = deepcopy(best_individual)
 
-        for _ in range(0, (population_size/2)):
+        for _ in range(0, int(population_size / 2)):
             a = do_tournament(population)
             b = do_tournament(population)
 
-            a_child, b_child = a.crossover_individual(b)
-            if a_child is not None and b_child is not None:
+            c, d = a.crossover_individual(b)
+            if c is not None and d is not None:
                 crossover_count += 1
-                fitness_calculator([a_child, b_child], data)
-                descent = [a_child, b_child, a, b]
+                for child in [c, d]:
+                    child.fitness_individual(data)
+                descent = [c, d, a, b]
                 descent.sort(key=lambda k: k.fitness)
-                if descent[0] == (a_child or b_child):
-                    best_children += 1  # I'm generating better children after crossbreed
-                if descent[1] == (a_child or b_child):
-                    best_children += 1
+                if descent[0] == (c or d):
+                    number_best_children += 1  # I'm generating better children after crossbreed
+                if descent[1] == (c or d):
+                    number_best_children += 1
                 if descent[0] or descent[1] not in new_population:
                     new_population.append(descent[0] or descent[1])
 
@@ -346,23 +354,35 @@ def run():
         # Using elitism to put the last best individual in the new population
 
         # Complete the population
-        new_population.extend(create_population(population_size - len(new_population)))
-        evolutionary_chart()
+        population_r = create_population(population_size - len(new_population))
+        new_population.extend(population_r)
+        evolutionary_chart(best_evolution, average_evolution, worst_evolution, children_evolution)
 
 
-def evolutionary_chart():
+def write_output():
+    # Write Output
+    with open("tests/circle.txt", "w") as archive:
+        archive.write("Variáveis analisadas: " + str(number_data) + "\n")
+        archive.write("Tamanho da População: " + str(population_size) + "\n")
+        archive.write("Probabilidade - Crossover: " + str(alfa_crossover) + "\n")
+        archive.write("Probabilidade - Mutação: " + str(beta_mutation) + "\n")
+        archive.write("Tamanho - Torneio: " + str(tournament_size) + "\n")
+        archive.write("Geração > Melhor Fitness > Fitness Médio > Melhor Indivíduo")
+
+
+def evolutionary_chart(lb, la, lw, lc):
     evolution_fig, evolution_ax = plot.subplots(4)
     evolution_fig.subplots_adjust(hspace=0.2)
 
-    y1 = np.array(worst_evolution)
-    y2 = np.array(average_evolution)
-    y3 = np.array(best_evolution)
-    y4 = np.array(best_children_gen)
+    y1 = np.array(lw)
+    y2 = np.array(la)
+    y3 = np.array(lb)
+    y4 = np.array(lc)
 
-    x1 = np.array(range(0, len(worst_evolution)))
-    x2 = np.array(range(0, len(average_evolution)))
-    x3 = np.array(range(0, len(best_evolution)))
-    x4 = np.array(range(0, len(best_children_gen)))
+    x1 = np.array(range(0, len(lw)))
+    x2 = np.array(range(0, len(la)))
+    x3 = np.array(range(0, len(lb)))
+    x4 = np.array(range(0, len(lc)))
 
     plot.setp(evolution_ax[0].get_xticklabels(), visible=False)
     plot.setp(evolution_ax[1].get_xticklabels(), visible=False)
@@ -380,15 +400,16 @@ def evolutionary_chart():
     evolution_ax[2].scatter(x2, y2, color='b', s=5.0)
     evolution_ax[3].scatter(x3, y3, color='g', s=5.0)
 
-    evolution_fig.savefig(sys.argv[2] + "_evolution.png")
+    evolution_fig.savefig("tests/circle" + "_evolution.png")
+    plot.close(evolution_fig)
 
 
 def clear_all_statistics():
-    global best_evolution, worst_evolution, average_evolution, best_children_gen
+    global best_evolution, worst_evolution, average_evolution, number_best_children
     best_evolution = []
     worst_evolution = []
     average_evolution = []
-    best_children_gen = []
+    number_best_children = []
 
 
 def result(lb, la, lw, lc):
@@ -402,14 +423,14 @@ def result(lb, la, lw, lc):
     ind_wor = [[row[r] for row in lw] for r in range(0, generations)]
     ind_chl = [[row[r] for row in lc] for r in range(0, generations)]
 
-    for i in range(0, generations):
-        best.append(sum(ind_best[i])/executions)
-        average.append(sum(ind_ave[i])/executions)
-        worst.append(sum(ind_wor[i])/executions)
-        children.append(sum(ind_chl[i])/executions)
+    for gen in range(0, generations):
+        best.append(sum(ind_best[gen]) / executions)
+        average.append(sum(ind_ave[gen]) / executions)
+        worst.append(sum(ind_wor[gen]) / executions)
+        children.append(sum(ind_chl[gen]) / executions)
 
         # Write Output
-    with open(sys.argv[2] + "_result.txt", "w") as archive:
+    with open("tests/circle" + "_result.txt", "w") as archive:
         archive.write("Variáveis analisadas: " + str(number_data) + "\n")
         archive.write("Tamanho da População: " + str(population_size) + "\n")
         archive.write("Probabilidade - Crossover: " + str(alfa_crossover) + "\n")
@@ -417,25 +438,30 @@ def result(lb, la, lw, lc):
         archive.write("Tamanho - Torneio: " + str(tournament_size) + "\n")
         archive.write("Média Aritmética das Execuções \n")
         archive.write("Geração > Melhor Fitness > Fitness Médio > Melhor Novo Indivíduo \n")
-        for i in range(0, generations):
-            archive.write("\n" + str(i + 1) + " > " + str(best[i]) + " > " + str(average[i]) +
-                          " > " + str(worst[i]) + " > " + str(children[i]))
+        for ind in range(0, generations):
+            archive.write("\n" + str(ind + 1) + " > " + str(best[ind]) + " > " + str(average[ind]) +
+                          " > " + str(worst[ind]) + " > " + str(children[ind]))
 
     evolutionary_chart(best, average, worst, children)
 
 
-if __name__ == '__main__':
+def main():
     best_all = []
     average_all = []
     worst_all = []
     children_proportion = []
 
-    for _ in range(0, executions):
+    for i in range(0, executions):
         run()
         best_all.append(best_evolution)
         average_all.append(average_evolution)
         worst_all.append(worst_evolution)
-        children_proportion.append(best_children_gen)
+        children_proportion.append(number_best_children)
         clear_all_statistics()
 
     result(best_all, average_all, worst_all, children_proportion)
+
+
+if __name__ == '__main__':
+    main()
+    print("finished - look at results :)")
